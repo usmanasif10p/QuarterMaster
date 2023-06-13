40e6557e985fa76e8a8153c455d25954f12340b9
Bilal Asif <muhammadbilal.asif@broadcom.com>
2023-06-13 14:27:03 +0500

    init project

---
+++ QuarterMaster/QuarterMaster.h
@@ -0,0 +1,18 @@
+//
+//  QuarterMaster.h
+//  QuarterMaster
+//
+//  Created by BilalAsif on 13/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuarterMaster.
+FOUNDATION_EXPORT double QuarterMasterVersionNumber;
+
+//! Project version string for QuarterMaster.
+FOUNDATION_EXPORT const unsigned char QuarterMasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuarterMaster/PublicHeader.h>
+
+
---
+++ QuarterMaster/QuarterMaster.swift
@@ -0,0 +1,22 @@
+//
+//  QuarterMaster.swift
+//  QuarterMaster
+//
+//  Created by BilalAsif on 13/06/2023.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
